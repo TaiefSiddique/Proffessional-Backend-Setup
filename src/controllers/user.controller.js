75feadf0555028f0b2402ddb23221d0c3c3f67e4
Md. Abu Taief Siddique <103975114+TaiefSiddique@users.noreply.github.com>
2024-03-11 09:40:27 +0600

    Update user.controller.js

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -237,6 +237,38 @@ const updateAccountDetails = asyncHandler(async (req, res) => {
         .status(200)
         .json(new ApiResponse(200, user, "Account details updated successfully"))
 });
+const updateUserAvatar = asyncHandler(async(req, res) => {
+    const avatarLocalPath = req.file?.path
+
+    if (!avatarLocalPath) {
+        throw new ApiError(400, "Avatar file is missing")
+    }
+
+    //TODO: delete old image - assignment
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+
+    if (!avatar.url) {
+        throw new ApiError(400, "Error while uploading on avatar")
+        
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set:{
+                avatar: avatar.url
+            }
+        },
+        {new: true}
+    ).select("-password")
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, user, "Avatar image updated successfully")
+    )
+})
 
 export {
     registerUser,
@@ -244,6 +276,7 @@ export {
     logoutUser,
     refreshAccessToken,
     changeCurrentPassword,
-    updateAccountDetails
+    updateAccountDetails,
+    updateUserAvatar
 
 }
